b6e932929e35d9eedab7ffc82545c7f80fbb1712
MarkSackerberg <93528482+MarkSackerberg@users.noreply.github.com>
2023-07-11 20:02:00 +0200

    add header option

--- mintText.tsx
+++ mintText.tsx
@@ -1,5 +1,6 @@
 
 export const mintText = [
-  { label: "allowl", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!" },
-  { label: "secondsGuardsLabel", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!" },
+  { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
+  { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
+  { label: "publi", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
 ];
