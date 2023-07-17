bd3dbcd00099c0cd1c89641bcc4c4987ce104ee4
MarkSackerberg <93528482+MarkSackerberg@users.noreply.github.com>
2023-07-17 22:10:58 +0200

    clarifications

--- allowlist.tsx
+++ allowlist.tsx
@@ -1,3 +1,5 @@
+//add your allowlists here. They need to be in the same format as you used to generate your merkle root. 
+//You can also add them here and copy the route from the console log 
 export const allowLists = new Map<string, Array<string>> ([
     ["WL",
     [
