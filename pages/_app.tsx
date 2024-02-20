119285b2f8722e9d6ce5351330b8a16ed33a16c1
MarkSackerberg <93528482+MarkSackerberg@users.noreply.github.com>
2024-02-20 02:28:36 +0100

    Fix typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,9 +33,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:title" content={headerText} />
         <meta
           property="og:description"
-          content="Website is based on MarkSackerbers work"
+          content="Website is based on Mark Sackerbergs work"
         />
-        <meta name="description" content="Website is based on MarkSackerbers work" />
+        <meta name="description" content="Website is based on Mark Sackerbergs work" />
 
         <meta
           property="og:image"
