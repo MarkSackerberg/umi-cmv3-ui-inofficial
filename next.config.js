41dbe0635794a1b334482262c7669c189c059816
MarkSackerberg <93528482+MarkSackerberg@users.noreply.github.com>
2025-12-04 14:10:13 +0100

    update packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: path.join(__dirname, './'),
 }
 
 module.exports = nextConfig
